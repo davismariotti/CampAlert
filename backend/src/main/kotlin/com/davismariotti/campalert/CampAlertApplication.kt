0f21c3bb7d33e7aeebd6a0744a2a750a25de5bec
Davis Mariotti <davismariotti@gmail.com>
2026-06-12 23:15:27 -0700

    feat: add ShedLock distributed lock to availability checker
    Enable @EnableSchedulerLock on the application class with JdbcTemplateLockProvider.
    Annotate AvailabilityChecker.processSearchRequests with @SchedulerLock
    (lockAtMostFor=PT90S, lockAtLeastFor=PT30S).

--- backend/src/main/kotlin/com/davismariotti/campalert/CampAlertApplication.kt
+++ backend/src/main/kotlin/com/davismariotti/campalert/CampAlertApplication.kt
@@ -1,10 +1,26 @@
 package com.davismariotti.campalert
 
+import net.javacrumbs.shedlock.core.LockProvider
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.jdbc.core.JdbcTemplate
+import javax.sql.DataSource
 
 @SpringBootApplication
-class CampFinderApplication
+@EnableSchedulerLock(defaultLockAtMostFor = "PT90S")
+class CampFinderApplication {
+    @Bean
+    fun lockProvider(dataSource: DataSource): LockProvider =
+        JdbcTemplateLockProvider(
+            JdbcTemplateLockProvider.Configuration.builder()
+                .withJdbcTemplate(JdbcTemplate(dataSource))
+                .usingDbTime()
+                .build(),
+        )
+}
 
 fun main(args: Array<String>) {
     runApplication<CampFinderApplication>(*args)
